6ad283018932d70850dc8c1e75871cb3f2b1ab78
Jeff Ilgen <jilgen@Jeffs-MacBook-Pro.local>
2013-03-26 10:48:33 -0400

    Attempted to get signal strength - failed

--- In3rds/src/com/example/in3rds/MainActivity.java
+++ In3rds/src/com/example/in3rds/MainActivity.java
@@ -1,15 +1,41 @@
 package com.example.in3rds;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.app.Activity;
+import android.content.Context;
 import android.view.Menu;
+import android.widget.TextView;
+import android.telephony.TelephonyManager;
+import android.telephony.PhoneStateListener;
 
 public class MainActivity extends Activity {
+	TextView textDisplay;
 
+	static final String TAG = "In3rds";
+	TelephonyManager telephonyManager;
+	PhoneStateListener phoneListener;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		textDisplay = (TextView) findViewById(R.id.textView1);
+		
+		//create Broadcast Receiver
+		///intent filters
+		//battery
+		
+		//get signal strength
+		phoneListener=new android.telephony.PhoneStateListener() {
+		     public void onSignalStrengthsChanged(int asu) { 
+		    	 Log.d(TAG, "Signal Strength "+asu );
+		    	 textDisplay.setText( ""+asu );
+		     }
+		};
+		telephonyManager = (TelephonyManager)this.getSystemService(Context.TELEPHONY_SERVICE);
+		Log.d(TAG, "string" );
+		telephonyManager.listen(phoneListener, PhoneStateListener.LISTEN_SIGNAL_STRENGTHS );
 	}
 
 	@Override
@@ -20,3 +46,5 @@ public class MainActivity extends Activity {
 	}
 
 }
+
+
