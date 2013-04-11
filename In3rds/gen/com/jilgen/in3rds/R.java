2f2e38d6c883a5b5dd72dfbe4508ce51ae230d08
jilgen <jilgen@192.168.1.107>
2013-04-11 09:07:04 -0400

    Added Battery history

--- In3rds/gen/com/jilgen/in3rds/R.java
+++ In3rds/gen/com/jilgen/in3rds/R.java
@@ -24,10 +24,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
+        public static final int action_settings=0x7f080006;
+        public static final int batteryHistoryView=0x7f080005;
         public static final int button1=0x7f080000;
-        public static final int textView1=0x7f080001;
-        public static final int textView2=0x7f080002;
+        public static final int textViewBattery=0x7f080001;
+        public static final int textViewLocation=0x7f080003;
+        public static final int textViewSignal=0x7f080002;
+        public static final int toggleButton1=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -38,8 +41,11 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int battery_switch=0x7f050004;
         public static final int hello_world=0x7f050002;
+        public static final int location_switch=0x7f050006;
         public static final int poll_button=0x7f050003;
+        public static final int signal_switch=0x7f050005;
     }
     public static final class style {
         /** 
--- In3rds/src/com/jilgen/in3rds/MainActivity.java
+++ In3rds/src/com/jilgen/in3rds/MainActivity.java
@@ -1,36 +1,74 @@
 package com.jilgen.in3rds;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.widget.TextView;
 import com.jilgen.in3rds.InternalStatistics;
 import com.jilgen.in3rds.SignalStrengthListener;
+import com.jilgen.in3rds.LocationService;
 import android.widget.Button;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 import android.view.View;
 import android.view.View.OnClickListener;
-
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Messenger;
+import com.jilgen.in3rds.BatteryValues;
+import java.lang.Thread;
 
 public class MainActivity extends Activity implements OnClickListener {
 	TextView textDisplay;
-	TextView statDisplay;
+	TextView locationDisplay;
+	TextView signalDisplay;
+	TextView batteryDisplay;
+	TextView batteryHistory;
 
 	static final String TAG = "In3rds";
 	InternalStatistics internalStatistics;
 	SignalStrengthListener signalStrength;
 	BatteryStrength batteryStrength;
+	boolean isBound;
+	LocationManager locationManager;
+	Double longitude, latitude;
+	BatteryValues batteryValues = new BatteryValues();
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		textDisplay = (TextView) findViewById(R.id.textView1);
-		statDisplay = (TextView) findViewById(R.id.textView2);
+		signalDisplay = (TextView) findViewById(R.id.textViewSignal);
+		batteryDisplay = (TextView) findViewById(R.id.textViewBattery);	
+		batteryHistory = (TextView) findViewById(R.id.batteryHistoryView);
+		
         Button button = (Button)findViewById(R.id.button1);
         button.setOnClickListener(this);
-		textDisplay.setText("Started \n");
 		signalStrength=new SignalStrengthListener( this );
 		batteryStrength=new BatteryStrength( this );
+		
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		final boolean gpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+		
+	    if (!gpsEnabled) {
+	        // Build an alert dialog here that requests that the user enable
+	        // the location services, then when the user clicks the "OK" button,
+	        // call enableLocationSettings()
+	    	Toast toast = Toast.makeText(this, "GPS not enabled", Toast.LENGTH_SHORT);
+	    	toast.show();
+	    } else {
+	    	locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+	    	        10000,          // 10-second interval.
+	    	        10,             // 10 meters.
+	    	        listener);
+	    }
+	    
 	}
 
 	@Override
@@ -39,10 +77,61 @@ public class MainActivity extends Activity implements OnClickListener {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	private final LocationListener listener = new LocationListener( ) {
+		
+		public void onLocationChanged(final Location location) { 
+			locationDisplay = (TextView) findViewById(R.id.textViewLocation);
+			
+			// TODO this is where you'd do something like context.sendBroadcast()
+			Log.d(TAG, "Latitude: "+location.getLatitude()
+					  +" Longitude: "+location.getLongitude()
+					  +" Speed: "+location.getSpeed());
+			latitude = location.getLatitude();
+			longitude = location.getLongitude();
+			String locDump=location.toString().replaceAll(",", "\n");
+			locationDisplay.setText( "Lat: "+latitude+"\nLong: "+longitude+"\n"+locDump);
+		}
+		
+		public void onProviderEnabled( String string ) {
+		}
+		
+		public void onProviderDisabled( String string ) {
+		}
+		
+		public void onStatusChanged( String string, int flag, Bundle bundle ) {
+		}
+	};
+	
 
-    public void onClick(View v) {
-		statDisplay.setText( "Signal: "+this.signalStrength.value+" Battery: "+this.batteryStrength.value );
+    public void onClick(View view) {
+		//signalDisplay.setText( "Signal: "+this.signalStrength.value+" Battery: "+this.batteryStrength.value );
+		signalDisplay.setText( "Signal: "+this.signalStrength.value );
+		batteryDisplay.setText("Battery: "+this.batteryStrength.value );
+		String batteryString = ""+this.batteryStrength.value;
+		this.batteryValues.addValue(batteryString);
+		Log.d(TAG, batteryValues.toString());
+		while (true) {
+			batteryHistory.setText(batteryValues.toString());
+			Thread.sleep(1000);
+		}
     }
+    
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+                String contents = intent.getStringExtra("SCAN_RESULT");
+                String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+                Log.d(TAG, contents+" "+format);
+                // Handle successful scan
+            } else if (resultCode == RESULT_CANCELED) {
+                // Handle cancel
+            }
+        }
+    }
+    
+    public void onToggleClicked(View view) {
+    	Log.d(TAG, "Toggle clicked");
+    }
+    
 }
-
-
